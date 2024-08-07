deb8f222e9d11258c1fd4ea7a9ce7f1e67e965b7
Lucas Oliveira <lucas.g.oliveira1998@outlook.com>
2024-08-07 00:41:15 -0300

    Adicionando Logo do projeto na tela de Login e na tela de Registro

--- app/src/main/java/com/example/conectatorcedor/ui/LoginPage.kt
+++ app/src/main/java/com/example/conectatorcedor/ui/LoginPage.kt
@@ -2,9 +2,11 @@ package com.example.conectatorcedor.ui
 
 import android.app.Activity
 import android.content.Intent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -22,10 +24,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.conectatorcedor.R
 import com.example.conectatorcedor.RegisterActivity
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -43,10 +46,12 @@ fun LoginPage(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Conecta Torcedor",
-            fontSize = 24.sp
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Conecta Torcedor Logo",
+            modifier = Modifier.fillMaxWidth()
         )
+
         Spacer(modifier = Modifier.height(50.dp))
         OutlinedTextField(
             value = email,
--- app/src/main/java/com/example/conectatorcedor/ui/RegisterPage.kt
+++ app/src/main/java/com/example/conectatorcedor/ui/RegisterPage.kt
@@ -1,20 +1,19 @@
 package com.example.conectatorcedor.ui
 
 import android.app.Activity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,11 +24,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.conectatorcedor.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
@@ -52,6 +50,13 @@ fun RegisterPage(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Conecta Torcedor Logo",
+            modifier = modifier.fillMaxWidth()
+        )
+        Spacer(modifier = modifier.height(50.dp))
+
         Text(text = "Registre-se")
         Spacer(modifier = modifier.height(50.dp))
         
